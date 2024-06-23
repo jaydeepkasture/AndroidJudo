092fc8af601d93a2b33504025376d0446af3ae31
Jaydeep Kasture <jaydeepkasture7@gmail.com>
2024-06-23 13:06:52 +0530

    path added

--- app/src/main/java/com/example/imagemovement/MainActivity.kt
+++ app/src/main/java/com/example/imagemovement/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.imagemovement
 
 import android.animation.ObjectAnimator
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import androidx.gridlayout.widget.GridLayout
 import android.widget.ImageView
+import androidx.gridlayout.widget.GridLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -20,9 +22,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pathform: ImageView
     private lateinit var binding: ActivityMainBinding
     private lateinit var gridLayout: GridLayout
-    private lateinit var spots: Array<ImageView>
-    private lateinit var spawnedImageView: ImageView
+    private lateinit var spots: Array<TextView>
+    private lateinit var spawnedTextView: TextView
     private lateinit var constraintLayout: ConstraintLayout
+    val bluepath= intArrayOf(133,132,131,130,129,143,158,173,188,203,218,217,216,201,186,171,156,141,125,124,123,122,121,120,105,90,91,92,93,94,95,81,66,51,36,21,6,7,8,23,38,53,68,83,99,100,101,103,104,119,118,117,116,115,114,113)
+     val yellowPath= intArrayOf( 23,38,53,68,83,99,100,101,102,103,104,119,134,133,132,131,130,129,143,158,173,188,203,218,217,216,201,186,171,156,141,125,124,123,122,121,120,105,90,91,92,93,94,95,81,66,51,36,21,6,7,22,37,52,67,82,97);
+ val redpath= intArrayOf(201,186,171,156,141,125,124,123,122,121,120,105,90,91,92,93,94,95,81,66,51,36,21,6,7,8,23,38,53,68,83,99,100,101,102,103,104,119,134,133,132,131,130,129,143,158,173,188,203,218,217,202,187,172,157,142,127)
+  val greenpath= intArrayOf(91,92,93,94,95,81,66,51,36,21,6,7,8,23,38,53,68,83,99,100,101,102,103,104,119,134,133,132,131,130,129,143,158,173,188,203,218,217,216,201,186,171,156,141,125,124,123,122,121,120,105,106,107,108,109,110,111)
     private  var tag: String="Board"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +41,15 @@ class MainActivity : AppCompatActivity() {
             // Ensure GridLayout has children and get the position of the first child
             gridLayout.post {
 
+                for (index in 0 until gridLayout.childCount) {
+                    var txtview=gridLayout.getChildAt(index) as TextView
+                    txtview.text=index.toString()
+                    txtview.setTextColor(Color.parseColor("#FFFFFF"))
+                    txtview.textAlignment=TextView.TEXT_ALIGNMENT_CENTER;
+                    txtview.setTextSize(10f)
+                    txtview.gravity = Gravity.CENTER
+                    txtview.setTextIsSelectable(true)
+                }
                 var child=gridLayout.getChildAt(0)
                 var y = child.width
                 var x = child.height;
@@ -47,8 +62,7 @@ class MainActivity : AppCompatActivity() {
 
                 constraintLayout = findViewById(R.id.main_layout)
                 Log.d(tag,"height $y widht $x")
-                val myArray1 = intArrayOf(1,  46,90,3,20,34,120,20,101,40,94,120,144)
-                    moveImageToSpots(myArray1) // Pass the array of indices
+               moveImageToSpots(greenpath) // Pass the array of indices
 
             }
         }catch (e:Exception){
